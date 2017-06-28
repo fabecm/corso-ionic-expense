e22b39e88b52c0a08c923453df9a4e409a8b877b
Fabio Bellini <fabio.bellini@crispybacon.it>
2017-06-28 14:18:48 +0200

    Start Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { DetailPage } from '../detail/detail';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController) {
 
   }
+
+  goToDetail (spesa) {
+    this.navCtrl.push(DetailPage);
+  }
+
+  expenses = [{
+    data: '2017-05-03',
+    testo: 'Biglietto per Venezia',
+    categoria: 'Viaggi',
+    spesa: 26
+  }, {
+    data: '2017-02-17',
+    testo: 'Concerto',
+    categoria: 'Svago',
+    spesa: 82
+  }, {
+    data: '2017-01-23',
+    testo: 'Ingresso presepe vivente',
+    categoria: 'Viaggi',
+    spesa: 100
+  }]
 
 }
